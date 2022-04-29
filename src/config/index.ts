d2b34a4188becf17a0492563924f5f0bd8bb3283
italo041 <italo_fernando11@hotmail.com>
2022-04-29 10:59:08 -0500

    add database config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const config = {
   server: {
     PORT: process.env.PORT || 3000,
+  },
+  database: {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME,
   },
 };
 
